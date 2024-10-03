c98512e5e7bfd71eeb391e68232c8d2da18f9d00
Josue Aguilar <josueaguilarjnai@gmail.com>
2024-10-03 02:19:32 -0600

    feat: create first page on nests@

--- src/app/pages/nests/nests.component.ts
+++ src/app/pages/nests/nests.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
-import {CommonModule} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {CommonModule, Location, NgOptimizedImage} from "@angular/common";
+import {Router, RouterLink} from "@angular/router";
+import {SensorsRes} from "../../model/sensorsRes";
+import {SensorsControllerService} from "../../services/sensorsController.service";
+import {LocalStorageService} from "../../local-storage.service";
 
 @Component({
   selector: 'app-nests',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    NgOptimizedImage,
+    RouterLink
 
   ],
   templateUrl: './nests.component.html',
   styleUrl: './nests.component.css'
 })
-export class NestsComponent {
+export class NestsComponent implements OnInit {
+  protected currentUser: any;
+  protected sensors: SensorsRes[] = [];
 
+
+  constructor(private location: Location,
+              private sensorService: SensorsControllerService,
+              private storage: LocalStorageService,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.currentUser = this.storage.currentUser;
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
